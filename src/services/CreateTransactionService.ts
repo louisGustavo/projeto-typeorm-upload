3b7ecd832c7fdb596f9596d0d67b536c65ce9bd0
Luís Gustavo <luis.gustavo_18@outlook.com>
2020-07-31 07:51:08 -0300

    Listagem e criação de uma transação simples

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,37 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import TransactionsRepository from '../repositories/TransactionsRepository'
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category
+  }:Request): Promise<Transaction> {
+
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
+
   }
 }
 
